16b4ccf5979705498e483103731faa17403e1529
andzia0520 <ankadaniluk@gmail.com>
2021-03-05 10:46:15 +0100

    settings

---
+++ src/main/java/com/kodilla/stock_trading_platform_front/StockTradingPlatformFrontApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.stock_trading_platform_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTradingPlatformFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockTradingPlatformFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/stock_trading_platform_front/StockTradingPlatformFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.stock_trading_platform_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTradingPlatformFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
